be9f624916dc07ec21484b81fd68c87d39205640
Henry Su <henry.su2@icloud.com>
2021-05-08 17:57:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // card options
     const cardArray = [
         {
             name: 'fries',
@@ -50,18 +51,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
+    cardArray.sort(() => 0.5 - Math.random())
+
     const grid = document.querySelector('.grid')
-    var cardsChosen = []
-    var cardsChosenId = []
-    var cardsWon = []
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
 
     //create your board
     function createBoard() {
-        for (let i =0; i < cardArray.length; i++){
-            var card = document.createElement('img')
+        for (let i = 0; i < cardArray.length; i++){
+            const card = document.createElement('img')
             card.setAttribute('src', 'images/blank.png')
             card.setAttribute('data-id', i)
-            card.addEventListener('click', flipcard)
+            card.addEventListener('click', flipCard)
             grid.appendChild(card) //will add card to grid in html
         }
     }
@@ -73,10 +77,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const optionTwoId = cardsChosenId[1]
         if (cardsChosen[0] === cardsChosen[1]) {
             alert('You found a match')
-            cards[optionOneId].setAttribute('src', 'images/white.png')
-            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cards[optionOneId].setAttribute('src', 'Images/white.png')
+            cards[optionTwoId].setAttribute('src', 'Images/white.png')
             cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'Images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'Images/blank.png')
+            alert('Sorry, try again')
         }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        // if you collected all the cards, you won
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations! You found them all'
+        }
+
 
 
     }
